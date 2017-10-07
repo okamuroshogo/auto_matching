495b70c4d1dca2c15b331e50fdea8181896eb91f
motoki-shun <motoki-shun@kayac.com>
2017-10-07 17:48:09 +0900

    htmlを返す

--- server/functions/share/index.js
+++ server/functions/share/index.js
@@ -2,6 +2,27 @@
 
 require('dotenv').config();
 
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    hoge
+  </body>
+</html>
+`;
+
 exports.handler = (event, context, callback) => {
-  console.log(event);
+  console.log(event.pathParameters);
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body: html,
+  };
+  callback(null, response);
 };
