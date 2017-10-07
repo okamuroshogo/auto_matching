f6c78afb9324fd460001a8319c214e1f02ce8cc5
motoki-shun <motoki-shun@kayac.com>
2017-10-07 14:07:18 +0900

    add detail

---
+++ front/src/js/components/detail.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  .detail(v-on:click="changeSampleName") {{ sampleMessage }}
+</template>
+
+<script>
+  import { mapGetters, mapActions } from 'vuex';
+
+  export default {
+    name: 'detail',
+    computed: {
+      ...mapGetters([ 'sampleMessage' ])
+    },
+    methods: {
+      ...mapActions([ 'changeSampleName' ])
+    }
+  }
+</script>
