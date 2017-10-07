0edbf657f40878dd1a79f7c35370ff41c284b82f
serinuntius <wasab1gj@gmail.com>
2017-10-08 01:37:39 +0900

    wip

--- server/functions/matching/handler.js
+++ server/functions/matching/handler.js
@@ -70,38 +70,6 @@ const deleteUser = (user) => {
   });
 };
 
-const createImage = (user_image1, user_image2, matching_reasons) => {
-  return new Promise((resolve, reject) => {
-    const dir = `/tmp/${uuid.v4()}`; // 書き込み可能なのは/tmp以下だけ、かつ名前重複でエラー起きるので重複できないようにする。
-    const options = [
-      "-size 300x300", // 画像のサイズ。変更元をxc:whiteにしているため必要
-      "-font ipam.ttf", // TrueTypeFontファイルへのパス。好きなフォントのTTFを使えばよろし。
-      "-pointsize 24", // 文字サイズ
-      '-fill "#000000"', // 文字色
-      "-gravity center", // 文字の基準位置
-      `-annotate +0+0 "${matching_reasons.join(',')}"` // 文字。基準位置(もしくは左上)からの位置を調整できる。
-    ];
-    exec(`mkdir ${dir} && convert ${options.join(" ")} xc:white ${dir}/image.png`, (err, stdout, stderr) => {
-      if (err) {
-        callback(stderr.split("\n"));
-        return;
-      }
-      s3.putObject({
-        Bucket: "",
-        Key: "",
-        Body: fs.createReadStream(`${dir}/image.png`),
-        ContentEncoded: 'base64',
-        ContentType: 'image/png'
-      }, (ex) => {
-        if (ex) {
-          callback(ex);
-          return;
-        }
-        callback(null, "success");
-      });
-    });
-  });
-};
 
 
 const createMatching = () => {
