736ba656edf31bf6e35c16c6a7e33804b7fccac4
motoki-shun <motoki-shun@kayac.com>
2017-10-07 12:08:08 +0900

    add front, vue

---
+++ front/src/js/components/root.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  .root(v-on:click="changeSampleName") {{ sampleMessage }}
+</template>
+
+<script>
+  import { mapGetters, mapActions } from 'vuex';
+
+  export default {
+      name: 'root',
+      computed: {
+          ...mapGetters([ 'sampleMessage' ])
+      },
+      methods: {
+          ...mapActions([ 'changeSampleName' ])
+      }
+  }
+</script>
