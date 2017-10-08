b926838a6f34c603cbc6714891b33089b53c5db4
motoki-shun <motoki-shun@kayac.com>
2017-10-08 14:08:55 +0900

    twitter widgets

--- front/src/js/components/root.vue
+++ front/src/js/components/root.vue
@@ -3,6 +3,7 @@
     h1.logo kamatte
     p マッチング数
     p {{ matchingCount || 0 }} 件！
+    | <a class="twitter-timeline"  href="https://twitter.com/hashtag/kamatte_cc" data-widget-id="916891364859641857">#kamatte_cc のツイート</a>
 </template>
 
 <script>
@@ -22,3 +23,6 @@
     }
   }
 </script>
+
+<!-- twitter widgets -->
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
