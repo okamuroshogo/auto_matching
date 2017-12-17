9b7f59bdb65fe591b2e49b97c57f35af906575e3
okamu <shogo@okamu.ro>
2017-12-18 01:08:44 +0900

    初めてフッターをつくったー

--- front/src/js/store/index.js
+++ front/src/js/store/index.js
@@ -4,6 +4,7 @@ import Vuex from 'vuex';
 import * as actions from './actions';
 import * as mutations from './mutations';
 import * as getters from './getters';
+import * as sns from './sns';
 
 Vue.use(Vuex);
 
