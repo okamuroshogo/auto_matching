e9e1c9e07a68aa11c7030a0f106641c92e914cd8
motoki-shun <motoki-shun@kayac.com>
2017-12-23 17:55:16 +0900

    axios消す

--- front/src/js/entry.js
+++ front/src/js/entry.js
@@ -1,10 +1,7 @@
 import Vue from 'vue';
 import store from './store';
-import http from 'http';
 import router from './router.js';
 import App from './app.vue';
-
-Vue.use(http, { store });
 
 new Vue({
   el: '#app',
