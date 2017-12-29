ffe911c4f6a0c0b47302808202caf5e878ff1c54
okamuroshogo <admin@okamu.ro>
2017-12-29 11:09:15 +0900

    Topにtwitterのツイートボタンを追加

--- front/src/js/components/root.vue
+++ front/src/js/components/root.vue
@@ -5,6 +5,17 @@
     //- div.matching_count
       p マッチング数
       p {{ matchingCount || 0 }} 件！
+    div.kamatte_tittle
+      p かまってちゃんのためのマッチングサービス
+      p kamatteは、twitter上のかまってちゃんを
+      p 
+        span.text-title-bold 勝手にマッチングして、お店の手配まで
+        | するwebサービスです
+    div.kamatte_tweet
+      | <a href="https://twitter.com/intent/tweet?text=この文言をランダムに変えたい" data-size="large"> かまってツイートする</a>
+    div.kamatte_tweet
+      | <a href="https://twitter.com/intent/tweet?text=この文言をランダムに変えたい" data-size="large"> かまってツイートする</a>
+
     div.top_twitter
       | <a class="twitter-timeline" href="https://twitter.com/kamatte_cc?ref_src=twsrc%5Etfw" data-show-replies="true">マッチング中</a>
 </template>
